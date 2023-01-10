18c379d125956c4398e5480ef1b7f2d0215a5096
Mohammad Javad <56100300+mohammadjr7@users.noreply.github.com>
2023-01-10 11:30:07 +0330

    AuthService added.

--- Eshop_FinalProject/Services/AuthService.cs
+++ Eshop_FinalProject/Services/AuthService.cs
@@ -1,12 +1,59 @@
-﻿using System;
+﻿using FinalProject_ECommerceApplication.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FinalProject_ECommerceApplication.Models;
 
-namespace Eshop_FinalProject.Services
+namespace FinalProject_ECommerceApplication.Services
 {
     public class AuthService
     {
+        public static bool isAuth = false;
+
+        public static int Authenticate(string username, string password, AppDbContext context)
+        {
+            username = Clean(username);
+
+            var user = context.Users.Include(user => user.Role).FirstOrDefault(user => user.Username == username && user.Password == password);
+
+            if (user != null)
+            {
+                isAuth = true;
+                return user.Role.Id;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public static bool IsAdmin(string username, AppDbContext context)
+        {
+            username = Clean(username);
+
+            var user = context.Users.Include(user => user.Role).FirstOrDefault(user => user.Username == username);
+
+            if (user != null)
+            {
+                if (user.Role.Id == 1) 
+                {
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+
+        private static string Clean(string text)
+        {
+            text = text.Replace(" ", "");
+            text = text.ToLower();
+            return text;
+        }
+
+
     }
 }
